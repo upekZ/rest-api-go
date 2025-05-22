7689aa06961269abcb8722283476cb94cf819458
upekZ <weerasekarau@gmail.com>
2025-05-22 14:43:15 +0530

    More basic UTs - stage 2
    Signed-off-by: upekZ <weerasekarau@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/go-chi/chi/v5 v5.2.1
+	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/patrickmn/go-cache v2.1.0+incompatible
@@ -11,10 +12,16 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/services/service_test.go
+++ internal/services/service_test.go
@@ -3,11 +3,13 @@ package services
 import (
 	"context"
 	"encoding/json"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/upekZ/rest-api-go/internal/database/queries"
 	"github.com/upekZ/rest-api-go/internal/model"
 	"net/http"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -22,23 +24,23 @@ func (m *MockUserDB) CreateUser(ctx context.Context, user *model.UserEntity) err
 }
 
 func (m *MockUserDB) UpdateUser(ctx context.Context, uID string, user *model.UserEntity) error {
-	args := m.Called(uID, user)
-	return args.Error(1)
+	args := m.Called(ctx, uID, user)
+	return args.Error(0)
 }
 
 func (m *MockUserDB) DeleteUser(ctx context.Context, id string) error {
-	args := m.Called(id)
+	args := m.Called(ctx, id)
 	return args.Error(0)
 }
 
 func (m *MockUserDB) GetUserByID(ctx context.Context, id string) (*model.UserEntity, error) {
-	args := m.Called(id)
+	args := m.Called(ctx, id)
 	return args.Get(0).(*model.UserEntity), args.Error(1)
 }
 
-func (m *MockUserDB) GetUsers(context.Context) ([]queries.User, error) {
-	//ToDo
-	return nil, nil
+func (m *MockUserDB) GetUsers(ctx context.Context) ([]queries.User, error) {
+	args := m.Called(ctx)
+	return args.Get(0).([]queries.User), args.Error(1)
 }
 
 func (m *MockUserDB) IsEmailUnique(context.Context, string) (bool, error) {
@@ -151,4 +153,82 @@ func TestUserService_CreateUser(t *testing.T) {
 			mockWS.AssertExpectations(t)
 		})
 	}
+}
+
+func TestUserService_UpdateUser(t *testing.T) {
+	mockDB := &MockUserDB{}
+	mockWS := &MockWebSocketService{}
+	mockCache := &MockUserCache{}
+	service := NewUserService(mockDB, mockCache, mockWS)
+
+	user := &model.UserEntity{FirstName: "Updated", LastName: "User", Email: "updated@example.com", Phone: "1234567890", Age: 25, Status: "Active"}
+	userID := "user123"
+
+	mockDB.On("UpdateUser", mock.Anything, userID, user).Return(nil)
+
+	user, err := service.UpdateUser(context.Background(), userID, user)
+
+	assert.NoError(t, err)
+	mockDB.AssertExpectations(t)
+	mockWS.AssertExpectations(t)
+}
+
+func TestUserService_DeleteUser(t *testing.T) {
+	mockDB := &MockUserDB{}
+	mockWS := &MockWebSocketService{}
+	mockCache := &MockUserCache{}
+	service := NewUserService(mockDB, mockCache, mockWS)
+
+	userID := "user123"
+	mockDB.On("GetUserByID", mock.Anything, userID).Return(&model.UserEntity{
+		UID: "user123",
+	}, nil)
+	mockDB.On("DeleteUser", mock.Anything, userID).Return(nil)
+
+	_, err := service.DeleteUser(context.Background(), userID)
+
+	assert.NoError(t, err)
+	mockDB.AssertExpectations(t)
+	mockWS.AssertExpectations(t)
+}
+
+func TestUserService_GetUserByID(t *testing.T) {
+	mockDB := &MockUserDB{}
+	mockWS := &MockWebSocketService{}
+	mockCache := &MockUserCache{}
+	service := NewUserService(mockDB, mockCache, mockWS)
+
+	userID := "user123"
+	expectedUser := &model.UserEntity{FirstName: "first", LastName: "last", Email: "first.last@bad.com", Phone: "10110101032", Age: 30, Status: "Active"}
+
+	mockDB.On("GetUserByID", mock.Anything, userID).Return(expectedUser, nil)
+
+	user, err := service.GetUserByID(context.Background(), userID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUser, user)
+	mockDB.AssertExpectations(t)
+}
+
+func TestUserService_GetUsers(t *testing.T) {
+	mockDB := &MockUserDB{}
+	mockWS := &MockWebSocketService{}
+	mockCache := &MockUserCache{}
+	service := NewUserService(mockDB, mockCache, mockWS)
+
+	uId1 := pgtype.UUID{uuid.New(), true}
+	uId2 := pgtype.UUID{uuid.New(), true}
+
+	expectedUsers := []queries.User{
+		{Userid: uId1, FirstName: "userfirst", LastName: "userlast", Email: "firstmail@unittest.com"},
+		{Userid: uId2, FirstName: "newfirst", LastName: "newlast", Email: "secondmail@unittest.com"},
+	}
+
+	mockDB.On("GetUsers", mock.Anything).Return(expectedUsers, nil)
+
+	users, err := service.ListUsers(context.Background())
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUsers, users)
+	mockDB.AssertExpectations(t)
 }
